feb1357f26e4e2402d8f3fad69096babba6a576f
brennan2455 <killerbeebot@gmail.com>
2022-09-19 21:46:32 -0500

    Added Kappa sigma logo to idex and fixed a loading bug

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
   resources :ks_hubs
-  root 'kd_hubs#index'
+  root 'ks_hubs#index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
