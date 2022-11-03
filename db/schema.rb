f8921c863f9d4667f0c822eb2be30533367059b3
Mahirah Samah <mahirah.samah@tamu.edu>
2022-11-02 19:45:38 -0500

    create announcements done, with admin functionality

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_11_02_234509) do
+ActiveRecord::Schema.define(version: 2022_11_02_235940) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -45,6 +45,7 @@ ActiveRecord::Schema.define(version: 2022_11_02_234509) do
     t.string "phonenumber"
     t.string "pledgeclass"
     t.string "major"
+    t.string "username"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
