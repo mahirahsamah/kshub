5de4007eccc8a518f49b00b19be22afd1fc73e42
daperego <dannyaperego@tamu.edu>
2022-11-04 18:24:07 +0000

    Added CRUD html.erb's and create form is done

--- app/controllers/merch_controller.rb
+++ app/controllers/merch_controller.rb
@@ -1,4 +1,30 @@
 class MerchController < ApplicationController
     def index
+        @merches = Merch.all
     end
+
+    def show
+    end
+
+    def new
+        @merch = Merch.new
+    end
+
+    def edit
+    end
+
+    def create
+        @merch = Merch.new(merch_params)
+    end
+
+    def update
+    end
+
+    def destroy
+    end
+
+    private
+        def merch_params
+            params.require(:merch).permit(:name, :price, :link)
+        end
 end
