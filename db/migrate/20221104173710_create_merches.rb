0a02a568766af4a939e2df17f02ccf94bb4ad8f6
daperego <dannyaperego@tamu.edu>
2022-11-04 17:39:23 +0000

    Added merch db values

---
+++ db/migrate/20221104173710_create_merches.rb
@@ -0,0 +1,11 @@
+class CreateMerches < ActiveRecord::Migration[6.1]
+  def change
+    create_table :merches do |t|
+      t.string :name
+      t.float :price
+      t.string :link
+
+      t.timestamps
+    end
+  end
+end
