771f46f126cb56009aed6bf2ec4885d6c96c2ee0
Mahirah Samah <mahirah.samah@tamu.edu>
2022-11-05 00:28:10 -0500

    added profile page controller

---
+++ app/controllers/profile_page_controller.rb
@@ -0,0 +1,2 @@
+class ProfilePageController < ApplicationController
+end
--- config/routes.rb
+++ config/routes.rb
@@ -8,6 +8,7 @@ Rails.application.routes.draw do
     omniauth_callbacks: 'users/omniauth_callbacks'
   }
   resources :ks_hubs
+  resources :profile_page
   #root 'ks_hubs#index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
