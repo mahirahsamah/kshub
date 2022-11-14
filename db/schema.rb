acb8f6d138a877d5480ac306972df7b5b1a884d1
daperego <dannyaperego@tamu.edu>
2022-11-14 01:12:53 +0000

    Added admin priveleges to edit function for announcements and events

--- db/schema.rb
+++ db/schema.rb
@@ -42,13 +42,17 @@ ActiveRecord::Schema.define(version: 2022_11_08_163606) do
     t.datetime "updated_at", precision: 6, null: false
   end
 
+  create_table "merches", force: :cascade do |t|
+    t.string "name"
+    t.float "price"
+    t.string "link"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
-    t.string "full_name"
-    t.string "uid"
-    t.string "avatar_url"
-    t.string "provider"
     t.string "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
@@ -62,6 +66,10 @@ ActiveRecord::Schema.define(version: 2022_11_08_163606) do
     t.string "pledgeclass"
     t.string "major"
     t.string "username"
+    t.string "full_name"
+    t.string "uid"
+    t.string "avatar_url"
+    t.string "provider"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
