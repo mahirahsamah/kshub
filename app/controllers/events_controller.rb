172aeba3819398582f98bbba3a5fd552855da0c3
Mahirah Samah <mahirah.samah@tamu.edu>
2022-11-19 19:38:49 -0600

    check box created

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -65,6 +65,6 @@ class EventsController < ApplicationController
 
     # Only allow a list of trusted parameters through.
     def event_params
-      params.require(:event).permit(:title, :body, :location, :date, :organizer)
+      params.require(:event).permit(:title, :body, :location, :date, :organizer, :ann)
     end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_11_14_234917) do
+ActiveRecord::Schema.define(version: 2022_11_20_011616) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -59,6 +59,7 @@ ActiveRecord::Schema.define(version: 2022_11_14_234917) do
     t.bigint "user_id", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.boolean "ann"
     t.index ["user_id"], name: "index_events_on_user_id"
   end
 
