c9a86ac5d627f50d4f782be1140affc9f12e4f80
brennan2455 <killerbeebot@gmail.com>
2022-11-27 20:48:29 -0600

    added new controller and models for comments system

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_11_24_201526) do
+ActiveRecord::Schema.define(version: 2022_11_28_021927) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -48,6 +48,16 @@ ActiveRecord::Schema.define(version: 2022_11_24_201526) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.index ["user_id"], name: "index_announcements_on_user_id"
+  end
+
+  create_table "comments", force: :cascade do |t|
+    t.text "content", null: false
+    t.bigint "announcement_id", null: false
+    t.bigint "user_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["announcement_id"], name: "index_comments_on_announcement_id"
+    t.index ["user_id"], name: "index_comments_on_user_id"
   end
 
   create_table "events", force: :cascade do |t|
@@ -104,5 +114,7 @@ ActiveRecord::Schema.define(version: 2022_11_24_201526) do
   end
 
   add_foreign_key "announcements", "users"
+  add_foreign_key "comments", "announcements"
+  add_foreign_key "comments", "users"
   add_foreign_key "events", "users"
 end
